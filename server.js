1e68422e102009440bc9eefec8146f39daec0466
Jay Freestone <mail@jayfreestone.com>
2017-04-24 19:08:35 +0100

    Prevents a few obvious errors.

--- server.js
+++ server.js
@@ -1,42 +1,65 @@
 const http = require('http');
-
-// Listen on a specific host via the HOST environment variable
-// var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-
 const fs = require('fs');
 const mime = require('mime');
 const AWS = require('aws-sdk');
-// AWS.config.loadFromPath('./config.json');
 
-const s3Bucket = new AWS.S3({params: {Bucket: 'gauntlet-images'}});
+// Listen on a specific port via the PORT environment variable
+const port = process.env.PORT || 8080;
 
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
+// Downloads and saves  a file, then runs a callback function
+function download(url, dest, cb) {
+  const file = fs.createWriteStream(dest);
+  http.get(url, (response) => {
     response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
+    file.on('finish', () => {
+      // close() is async, call cb after close completes.
+      file.close(cb);
     });
   });
-};
+}
 
+// Checks if a domain passes
+function isValidDomain(url) {
+  const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+  const domain = matches && matches[1];
+  return domain === 'i.4cdn.org';
+}
+
+// Use a (local, ignored) config file if present
+// Needs to happen before bucket initialisation.
+if (fs.existsSync('./config.json')) {
+  AWS.config.loadFromPath('./config.json');
+}
+
+// Set up our bucket
+const s3Bucket = new AWS.S3({params: {Bucket: 'gauntlet-images'}});
+
+// Set up the request handler
 const requestHandler = (request, response) => {
   const image_url = request.url.slice(1);
   const image_file = image_url.substring(image_url.lastIndexOf('/') + 1);
   const image_path = `./tmp/${image_file}`;
   const mimeType = mime.lookup(image_path);
 
-  if (mimeType !== 'image/x-icon') {
+  // Ignore favicons
+  if (isValidDomain(image_url) && mimeType !== 'image/x-icon') {
+    // Check if we already have the file...
+    s3Bucket.headObject({ Key: image_file }).on('success', () => {
+      // Abort if we've already mirrored it
+      response.end('Already mirrored.');
+    }).send();
+
+    // ...otherwise start the download
     download(image_url, image_path, () => {
+      // Set up the params for S3
       const param_data = {
         Key: image_file,
         Body: fs.readFileSync(image_path),
         ContentType: mimeType,
       };
 
-      s3Bucket.upload(param_data, function(err, output_data) {
+      // Try and upload the image to S3
+      s3Bucket.upload(param_data, (err, output_data) => {
         if (err) {
           console.log('Error uploading data to S3: ' + err);
         } else {
@@ -44,16 +67,17 @@ const requestHandler = (request, response) => {
         }
       });
     });
+  } else {
+    response.end('Something went wrong.');
   }
-
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${port}`);
 });
